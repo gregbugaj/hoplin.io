3cdde734543e86bddb335366d17024e173272d47
greg <gregbugaj@yahoo.com>
2018-11-13 06:41:59 -0600

    Initial commit

---
+++ client/src/test/java/io/hoplin/AppTest.java
@@ -0,0 +1,15 @@
+package io.hoplin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest 
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ logreader/src/test/java/io/hoplin/AppTest.java
@@ -0,0 +1,20 @@
+package io.hoplin;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple Application.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
