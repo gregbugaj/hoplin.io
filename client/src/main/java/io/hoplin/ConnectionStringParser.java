451b59e6723d97cbcc8d82a35a9c6a7c3fc03668
greg <gregbugaj@yahoo.com>
2018-11-19 07:28:44 -0600

    WIP Connection string parsing

--- client/src/main/java/io/hoplin/ConnectionStringParser.java
+++ client/src/main/java/io/hoplin/ConnectionStringParser.java
@@ -1,27 +1,56 @@
 package io.hoplin;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class ConnectionStringParser
 {
-    private Map<String, OptionSetter> setters  = new HashMap<>();
-
-    public static void parse(final String connectionString)
+    public static RabbitMQOptions parse(final String connectionString)
     {
         final String[] parts = connectionString.split(";");
 
+        RabbitMQOptions options = new RabbitMQOptions();
         for(String part : parts)
         {
             final String[] kv = part.split("=");
             if(kv.length != 2)
                 throw new RuntimeException("Invalid KeyValue pair");
 
+            final String key = kv[0];
+            final String value = kv[1];
+
+            // host (e.g. host=localhost or host=192.168.2.56)
+            // virtualHost (e.g. virtualHost=myVirtualHost) default is the default virtual host '/'
+
+            if("host".equalsIgnoreCase(key))
+                options.setHost(value);
+            else if("virtualHost".equalsIgnoreCase(key))
+                options.setVirtualHost(value);
+            else if("username".equalsIgnoreCase(key))
+                options.setUser(value);
+            else if("password".equalsIgnoreCase(key))
+                options.setPassword(value); 
+            else if("requestedHeartbeat".equalsIgnoreCase(key))
+                options.setRequestedHeartbeat(getIntVal(value));
+            else if("prefetchcount".equalsIgnoreCase(key))
+                options.setRequestedHeartbeat(getIntVal(value));
+            else if("publisherConfirms".equalsIgnoreCase(key))
+                options.setRequestedHeartbeat(getIntVal(value));
+            else if("persistentMessages".equalsIgnoreCase(key))
+                options.setRequestedHeartbeat(getIntVal(value));
+            else if("timeout".equalsIgnoreCase(key))
+                options.setRequestedHeartbeat(getIntVal(value));
+            else if("product".equalsIgnoreCase(key))
+                options.setRequestedHeartbeat(getIntVal(value));
+            else if("platform".equalsIgnoreCase(key))
+                options.setRequestedHeartbeat(getIntVal(value));
+            else
+                throw new IllegalArgumentException("Unknown option : " + key);
         }
+
+        return options;
     }
 
-    private class OptionSetter
+    private static int getIntVal(final String value)
     {
-
+        return Integer.parseInt(value);
     }
+
 }
---
+++ client/src/test/java/io/hoplin/ConnectionStringParserTest.java
@@ -0,0 +1,14 @@
+package io.hoplin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConnectionStringParserTest {
+
+    @Test
+    void parse()
+    {
+
+    }
+}
